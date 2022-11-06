088fd9eacf454de63625518ddb79274e85077041
root <root@TORPEDO.localdomain>
2022-11-06 15:13:35 +0100

    feat: main functions

---
+++ src/typings.ts
@@ -0,0 +1,17 @@
+export interface PaginationArgs {
+  page?: number;
+  limit?: number;
+  search?: string;
+  from?: string;
+  to?: string;
+}
+
+export interface DateRangeArgs {
+  from?: string;
+  to?: string;
+}
+
+export interface PaginationOptions {
+  includes?: Record<string, boolean | any>;
+  search?: string[];
+}
